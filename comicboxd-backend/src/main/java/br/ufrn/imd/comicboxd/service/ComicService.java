1126235e66ebab6e67e9be4a1e5280a3019a9116
Klyyyf <klyf.if@gmail.com>
2026-01-29 11:17:16 -0300

    Feat: Finaliza CRUD de HQs (Read com paginação, Update e Delete)

--- comicboxd-backend/src/main/java/br/ufrn/imd/comicboxd/service/ComicService.java
+++ comicboxd-backend/src/main/java/br/ufrn/imd/comicboxd/service/ComicService.java
@@ -8,6 +8,8 @@ import br.ufrn.imd.comicboxd.repositories.ComicRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -38,5 +40,68 @@ public class ComicService {
 
         dto.setId(savedEntity.getId());
         return dto;
+    }
+
+    @Transactional
+    public ComicDTO update(Long id, ComicDTO dto) {
+        Comic entity = comicRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("HQ não encontrada com id:" + id));
+
+        entity.setTitle(dto.getTitle());
+        entity.setDescription(dto.getDescription());
+        entity.setCategory(dto.getCategory());
+        entity.setCoverUrl(dto.getCoverUrl());
+        entity.setReleaseDate(dto.getReleaseDate());
+
+        if (dto.getAuthorsIds() != null) {
+            List<Author> authorsFound = authorRepository.findAllById(dto.getAuthorsIds());
+
+            entity.setAuthors(authorsFound);
+        }
+
+        Comic savedEntity = comicRepository.save(entity);
+
+        return toDTO(savedEntity);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Comic entity = comicRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("HQ não encontrada com id:" + id));
+
+        comicRepository.delete(entity);
+    }
+
+    private ComicDTO toDTO(Comic comic) {
+        ComicDTO comicDTO = new ComicDTO();
+
+        comicDTO.setId(comic.getId());
+        comicDTO.setTitle(comic.getTitle());
+        comicDTO.setDescription(comic.getDescription());
+        comicDTO.setCategory(comic.getCategory());
+        comicDTO.setCoverUrl(comic.getCoverUrl());
+        comicDTO.setReleaseDate(comic.getReleaseDate());
+
+        if (comic.getAuthors() != null) {
+            List<Long> ids = comic.getAuthors().stream()
+                    .map(Author::getId)
+                    .toList();
+
+            comicDTO.setAuthorsIds(ids);
+        }
+
+        return comicDTO;
+    }
+
+    public Page<ComicDTO> findAll(Pageable pageable, String category) {
+        Page<Comic> comicPage;
+
+        if (category == null) {
+            comicPage = comicRepository.findAll(pageable);
+        } else {
+            comicPage = comicRepository.findByCategory(category, pageable);
+        }
+
+        return comicPage.map(this::toDTO);
     }
 }
