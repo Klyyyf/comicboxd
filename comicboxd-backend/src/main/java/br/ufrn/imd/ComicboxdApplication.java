9debc89411a2ab857cfb834c4afe601292b2c63c
Joaremio <joaremiorevoredo@gmail.com>
2026-01-28 22:58:46 -0300

    alterado packages no backend

--- comicboxd-backend/src/main/java/br/ufrn/imd/ComicboxdApplication.java
+++ comicboxd-backend/src/main/java/br/ufrn/imd/ComicboxdApplication.java
@@ -1,4 +1,4 @@
-package com.comicboxd;
+package br.ufrn.imd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- comicboxd-backend/src/main/java/br/ufrn/imd/model/Comic.java
+++ comicboxd-backend/src/main/java/br/ufrn/imd/model/Comic.java
@@ -1,4 +1,4 @@
-package com.comicboxd.domain.model;
+package br.ufrn.imd.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
